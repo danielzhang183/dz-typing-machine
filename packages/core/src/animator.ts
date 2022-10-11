82b241d67d6f806d00381958f37cbbc6248be7e2
danielzhang183 <danielzhang182@gmail.com>
2022-10-12 07:15:35 +0800

    chore: update deps\

--- packages/core/src/animator.ts
+++ packages/core/src/animator.ts
@@ -1,15 +1,15 @@
-import type { Patch } from './types'
+import type { Patch } from '.'
+
 export function* createAnimator(input: string, patches: Patch[]) {
   let output = input
   let cursor = 0
-
   for (const patch of patches) {
     if (patch.type === 'insert') {
       cursor = patch.from
       const head = output.slice(0, patch.from)
       const tail = output.slice(patch.from)
-      let selection = patch.text
-      for (const char of selection) {
+      let selection = ''
+      for (const char of patch.text) {
         selection += char
         yield {
           cursor: cursor + selection.length,
@@ -20,9 +20,9 @@ export function* createAnimator(input: string, patches: Patch[]) {
     }
     else if (patch.type === 'removal') {
       cursor = patch.from - patch.length
-      const head = output.slice(0, cursor)
+      const head = output.slice(0, patch.from - patch.length)
       const tail = output.slice(patch.from)
-      const selection = output.slice(cursor, patch.from)
+      const selection = output.slice(patch.from - patch.length, patch.from)
       for (let i = selection.length; i >= 0; i--) {
         yield {
           cursor: cursor + i,
@@ -31,9 +31,5 @@ export function* createAnimator(input: string, patches: Patch[]) {
       }
       output = head + tail
     }
-  }
-
-  return {
-    output,
   }
 }
